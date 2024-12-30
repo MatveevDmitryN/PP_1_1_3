b14670bc1bffc793822be3811f63114c741988ff
Дмитрий Матвеев <matveevdmitryn@gmail.com>
2024-12-31 00:54:28 +0300

    Complete the task

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,31 +1,47 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
+    private final UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
+
+    @Override
     public void createUsersTable() {
-
+        userDao.createUsersTable(); // Вызываем метод DAO
+        System.out.println("Таблица пользователей создана.");
     }
 
+    @Override
     public void dropUsersTable() {
-
+        userDao.dropUsersTable(); // Вызываем метод DAO
+        System.out.println("Таблица пользователей удалена.");
     }
 
+    @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        userDao.saveUser(name, lastName, age); // Вызываем метод DAO
+        System.out.println("Пользователь " + name + " " + lastName + " добавлен в базу данных.");
     }
 
+    @Override
     public void removeUserById(long id) {
-
+        userDao.removeUserById(id); // Вызываем метод DAO
+        System.out.println("Пользователь с ID " + id + " удалён из базы данных.");
     }
 
+    @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = userDao.getAllUsers(); // Вызываем метод DAO
+        System.out.println("Список пользователей получен.");
+        return users;
     }
 
+    @Override
     public void cleanUsersTable() {
-
+        userDao.cleanUsersTable(); // Вызываем метод DAO
+        System.out.println("Таблица пользователей очищена.");
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,20 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/mydbtest";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("Ошибка подключения к БД.");
+            throw new RuntimeException(e);
+        }
+    }
 }
